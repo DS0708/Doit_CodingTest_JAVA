444f3004a7a1dba2674ab8b1be5f1ce0d398f3ca
DS0708 <tlsehdtjd070@naver.com>
2024-01-05 21:20:42 +0900

    P002

---
+++ DataStructure/P002.java
@@ -0,0 +1,20 @@
+package DataStructure;
+
+import java.util.Scanner;
+
+public class P002 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        int sum = 0;
+        int max_score = Integer.MIN_VALUE;
+
+        for (int i=0; i<n; i++){
+            int num = scan.nextInt();
+            sum += num;
+            if (max_score < num) max_score = num;
+        }
+
+        System.out.println((sum * 100.0 / max_score / n));
+    }
+}
