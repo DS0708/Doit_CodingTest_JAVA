052dee09a2abb8b7d08f12ed66285d7352095908
DS0708 <tlsehdtjd070@naver.com>
2024-05-13 13:59:05 +0900

    조합 - 완전 순열 (Derangement)

--- Combination/P083_1947.java
+++ Combination/P083_1947.java
@@ -8,7 +8,9 @@ public class P083_1947 {
     int n = scan.nextInt();
     long[] derangement = new long[n+1];
     derangement[1] = 0;
-    derangement[2] = 1;
+
+    if(n >= 2)  //n이 1일 때 derangement[2] 는 없음
+      derangement[2] = 1;
 
     for(int i=3; i<=n; i++){
       derangement[i] = (i-1) * (derangement[i-1] + derangement[i-2]) % 1000000000;
