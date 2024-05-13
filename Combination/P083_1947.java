4b3c3c8052d6d029e7f8dc47652eb2c74f2cadea
DS0708 <tlsehdtjd070@naver.com>
2024-05-13 13:53:49 +0900

    조합 - 완전 순열 (Derangement)

---
+++ Combination/P083_1947.java
@@ -0,0 +1,21 @@
+package Combination;
+
+import java.util.Scanner;
+
+public class P083_1947 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    long[] derangement = new long[n+1];
+    derangement[1] = 0;
+    derangement[2] = 1;
+
+    for(int i=3; i<=n; i++){
+      derangement[i] = (i-1) * (derangement[i-1] + derangement[i-2]) % 1000000000;
+      // (a + b) mod c = (a mod c + b mod c) mod c
+      // (a * b) mod c = (a mod c * b mod c) mod c
+    }
+
+    System.out.println(derangement[n]);
+  }
+}
