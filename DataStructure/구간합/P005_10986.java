b1b509f8d877b7ad2a95c582ee537ea2aaf87813
DS0708 <tlsehdtjd070@naver.com>
2024-06-10 19:02:09 +0900

    구간합 - 응용 3

--- DataStructure/구간합/P005_10986.java
+++ DataStructure/구간합/P005_10986.java
@@ -20,7 +20,7 @@ public class P005_10986 {
         for (int i=1; i<=n; i++){
             sum_arr[i] = sum_arr[i-1] + Integer.parseInt(st.nextToken());
 
-            int rest = (int)(sum_arr[i] % m);
+            int rest = (int)(sum_arr[i] % m);     //sum_arr[i]는 long값이기 때문에 % 한 결과를 int로 변환해야 한다.
 
             if (rest ==0) result++;
 
@@ -30,7 +30,7 @@ public class P005_10986 {
         for (int i=0; i<m ;i++){
             if(rest_count_arr[i] == 0) continue;
 
-            result += rest_count_arr[i] * (rest_count_arr[i]-1) / 2;
+            result += rest_count_arr[i] * (rest_count_arr[i]-1) / 2;    //이 부분에서 int 범위를 벗어날 수 있어 result는 long으로 선언해야함
         }
 
         System.out.println(result);
