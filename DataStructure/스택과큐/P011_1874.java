81e75245ca864c75b9ea9d51cd3ef211677de1ad
DS0708 <tlsehdtjd070@naver.com>
2024-06-10 23:02:58 +0900

    스택 활용하기 - 수정

--- DataStructure/스택과큐/P011_1874.java
+++ DataStructure/스택과큐/P011_1874.java
@@ -1,44 +1,45 @@
 package DataStructure.스택과큐;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
+import static java.lang.System.exit;
 
 public class P011_1874 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int arr[] = new int[N];
-        int current = 1;
-        boolean result = true;
-
-        StringBuffer sb = new StringBuffer();
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i=0; i<N; i++){
-            arr[i] = scan.nextInt();
+  public static void main(String args[]) {
+    StringBuffer sb = new StringBuffer();
+    Scanner scan = new Scanner(System.in);
+    boolean check = true;
+    int n = scan.nextInt();
+    int count = 1;
+    Stack<Integer> stack = new Stack<>();
+    //하나씩 보기
+    for(int i=0; i<n; i++){
+      int cur = scan.nextInt();
+      //count가 cur될 때까지 push하고 한 번 pop
+      if(count <= cur){
+        for(;count <=cur ; count++){
+          stack.push(count);
+          sb.append("+\n");
         }
-
-        for (int i=0; i<N ; i++){
-            int now = arr[i];
-            if (now >= current){
-                while(now >= current){
-                    stack.push(current);
-                    current++;
-                    sb.append("+\n");
-                }
-                stack.pop();
-                sb.append("-\n");
-            }else {
-                if (stack.pop() > now){
-                    result = !result;
-                    System.out.println("NO");
-                    break;
-                }else {
-                    sb.append("-\n");
-                }
-            }
+        stack.pop();
+        sb.append("-\n");
+      }else{
+        if(stack.peek() == cur){
+          stack.pop();
+          sb.append("-\n");
+        }else{
+          check = false;
+          break;
         }
+      }
 
-        if (result) System.out.println(sb.toString());
     }
+    //출력 Logic
+    if(!check){
+      System.out.println("NO");
+      //그냥써본거임
+      exit(0);
+    }else{
+      System.out.println(sb);
+    }
+  }
 }
