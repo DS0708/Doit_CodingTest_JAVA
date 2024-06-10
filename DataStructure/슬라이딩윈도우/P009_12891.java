c9daaee95250ed237c086d31b7284390fde2181e
DS0708 <tlsehdtjd070@naver.com>
2024-06-10 21:22:15 +0900

    슬라이딩 윈도우 기초 - 더 쉬운 코드

--- DataStructure/슬라이딩윈도우/P009_12891.java
+++ DataStructure/슬라이딩윈도우/P009_12891.java
@@ -1,90 +1,41 @@
 package DataStructure.슬라이딩윈도우;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class P009_12891 {
-    static int[] MyArr;
-    static int[] CheckArr;
-    static int CheckSecret;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        char[] S_arr = br.readLine().toCharArray();
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int s = scan.nextInt();
+      int p = scan.nextInt();
+      int count = 0;
+      String str = scan.next();
+      int[] checkArr = new int[24];
+      int[] curArr = new int[24];
+      char[] strArr = str.toCharArray();
+      int indexA = 'A' - 'A';
+      int indexC = 'C' - 'A';
+      int indexG = 'G' - 'A';
+      int indexT = 'T' - 'A';
 
+      checkArr[indexA] = scan.nextInt();
+      checkArr[indexC] = scan.nextInt();
+      checkArr[indexG] = scan.nextInt();
+      checkArr[indexT] = scan.nextInt();
 
-        int result = 0;
-        CheckSecret = 0;
+      for(int i=0; i<p; i++){
+        curArr[strArr[i]-'A']++;
+      }
 
-        CheckArr = new int[4];
-        MyArr = new int[4];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i=0; i<4; i++){
-            CheckArr[i] = Integer.parseInt(st.nextToken());
-            if (CheckArr[i]==0) CheckSecret++;
+      for(int i=p-1; i<s; i++){
+        if(i!=p-1){
+          curArr[strArr[i-p]-'A']--;
+          curArr[strArr[i]-'A']++;
         }
-
-        // 초기 윈도우 추가
-        for (int i=0; i<P; i++){
-            Add(S_arr[i]);
+        if(curArr[indexA] >= checkArr[indexA] && curArr[indexC] >= checkArr[indexC] && curArr[indexG] >= checkArr[indexG] && curArr[indexT] >= checkArr[indexT]){
+          count ++;
         }
-        if (CheckSecret == 4) result++;
+      }
 
-        // 나머지 윈도우 수행
-        for (int i=P; i<S; i++){
-            Add(S_arr[i]);
-            Remove(S_arr[i-P]);
-            if (CheckSecret == 4) result++;
-        }
-
-        System.out.println(result);
-
-    }
-
-    private static void Remove(char c) {
-        switch (c){
-            case 'A' :
-                MyArr[0] --;
-                if (MyArr[0] == (CheckArr[0]-1)) CheckSecret --;
-                break;
-            case 'C' :
-                MyArr[1] --;
-                if (MyArr[1] == (CheckArr[1]-1)) CheckSecret --;
-                break;
-            case 'G' :
-                MyArr[2] --;
-                if (MyArr[2] == (CheckArr[2]-1)) CheckSecret --;
-                break;
-            case 'T' :
-                MyArr[3] --;
-                if (MyArr[3] == (CheckArr[3]-1)) CheckSecret --;
-                break;
-        }
-    }
-
-    private static void Add(char c) {
-        switch (c){
-            case 'A' :
-                MyArr[0] ++;
-                if (MyArr[0] == CheckArr[0]) CheckSecret ++;
-                break;
-            case 'C' :
-                MyArr[1] ++;
-                if (MyArr[1] == CheckArr[1]) CheckSecret ++;
-                break;
-            case 'G' :
-                MyArr[2] ++;
-                if (MyArr[2] == CheckArr[2]) CheckSecret ++;
-                break;
-            case 'T' :
-                MyArr[3] ++;
-                if (MyArr[3] == CheckArr[3]) CheckSecret ++;
-                break;
-        }
+      System.out.println(count);
     }
 }
