ab1da644ed1287be1c1145df77aff8850577d15d
DS0708 <tlsehdtjd070@naver.com>
2024-06-10 21:56:50 +0900

    슬라이딩 윈도우 플래티넘 문제 - Deque 자료구조 활용하기 (수정)

--- DataStructure/슬라이딩윈도우/P010_11003.java
+++ DataStructure/슬라이딩윈도우/P010_11003.java
@@ -6,56 +6,43 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class P010_11003 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
+  public static void main(String args[]) throws Exception{
+    //IO 연산이 많을 때는 버퍼리더와 버퍼라이터를 쓰지 않으면 시간 초과 걸림
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int L = Integer.parseInt(st.nextToken());
+    Deque<Node11003> myDeque = new LinkedList<>();
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Deque<Node> myDeque = new LinkedList<>();
+    st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
+    myDeque.addFirst(new Node11003(1,Integer.parseInt(st.nextToken())));
+    bw.write(myDeque.getFirst().val+" ");
 
-        //초기값 설정
-        long init_value = Integer.parseInt(st.nextToken());
-        myDeque.add(new Node(0,init_value));
-        bw.write(init_value+" ");
+    for(int i=2; i<=N; i++){
+      Node11003 newNode = new Node11003(i,Integer.parseInt(st.nextToken()));
 
-        //Logic
-        for (int i=1; i<N; i++){
-            //now 설정
-            long now_value = Long.parseLong(st.nextToken());
+      while (!myDeque.isEmpty() && myDeque.getLast().val > newNode.val)
+        myDeque.removeLast();
 
-            // now < 덱의 LastValue 이면 계속해서 덱의 마지막 인덱스 제거하기
-            while(!myDeque.isEmpty() && now_value < myDeque.getLast().value){
-                myDeque.removeLast();
-            }
+      myDeque.addLast(newNode);
 
-            //now_value 추가하기
-            myDeque.addLast(new Node(i,now_value));
+      while (!myDeque.isEmpty() && myDeque.getFirst().index <= newNode.index - L)
+        myDeque.removeFirst();
 
-            //인덱스 범위 확인
-            if (myDeque.getFirst().index <= myDeque.getLast().index - L)
-                myDeque.removeFirst();
-
-            //첫번째 덱의 value 추가
-            bw.write(myDeque.getFirst().value+" ");
-        }
-
-
-        bw.flush();
-        bw.close();
-        br.close();
+      bw.write(myDeque.getFirst().val+" ");
     }
 
-    static class Node{
-        int index;
-        long value;
-
-        public Node(int index, long value){
-            this.index = index;
-            this.value = value;
-        }
-    }
+    bw.flush();
+    bw.close();
+  }
+}
+class Node11003{
+  int index;
+  int val;
+  Node11003(int index, int val){
+    this.index = index;
+    this.val = val;
+  }
 }
