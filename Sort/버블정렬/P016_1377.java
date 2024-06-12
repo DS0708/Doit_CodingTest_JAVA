e73e112ae20638c94113fad513b089f77b7e6430
DS0708 <tlsehdtjd070@naver.com>
2024-06-12 13:50:31 +0900

    버블정렬 stop되는 구간 구하기

--- Sort/버블정렬/P016_1377.java
+++ Sort/버블정렬/P016_1377.java
@@ -7,37 +7,38 @@ import java.util.Arrays;
 
 public class P016_1377 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        mData[] A = new mData[N];
+      //스캐너로 하면 메모리 초과 문제도 있다.
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      MyData[] arr = new MyData[n];
 
-        for (int i=0; i<N ; i++){
-            A[i] = new mData(Integer.parseInt(br.readLine()),i);
-        }
+      for(int i=0; i<n; i++){
+        arr[i] = new MyData(Integer.parseInt(br.readLine()), i);
+      }
 
-        Arrays.sort(A);
+      Arrays.sort(arr);
 
-        int Max = 0;
-        for (int i=0; i<N; i++){
-            if (Max < A[i].index - i) Max = A[i].index-i;
-        }
+      int max = 0;
 
-        System.out.println(Max+1);
+      for(int i=0; i<n; i++){
+        max = Math.max(max, arr[i].index-i);
+      }
+
+      System.out.println(max+1);
     }
+}
 
-    static class mData implements Comparable<mData>{
-        int value;
-        int index;
+class MyData implements Comparable<MyData>{
+  int val;
+  int index;
 
-        public mData(int value, int index){
-            super();
-            this.value = value;
-            this.index = index;
-        }
+  MyData(int v, int i){
+    this.val = v;
+    this.index = i;
+  }
 
-        @Override
-        public int compareTo(mData o){ //value기준 오름차순 정렬하기
-            return this.value - o.value;
-        }
-    }
+  @Override
+  public int compareTo(MyData t){
+    return this.val - t.val;
+  }
 }
