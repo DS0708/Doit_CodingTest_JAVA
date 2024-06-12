dad7df3c80aa92e8c6b977ee14aa55b2f1a493a2
DS0708 <tlsehdtjd070@naver.com>
2024-06-12 13:06:26 +0900

    Queue - 우선순위 큐와 Comparator 구현하기

--- DataStructure/스택과큐/P014_11286.java
+++ DataStructure/스택과큐/P014_11286.java
@@ -3,41 +3,48 @@ package DataStructure.스택과큐;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class P014_11286 {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = scanner.nextInt();
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(((o1, o2) -> {
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt();
+//    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new pqComparator());
+      PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) ->{
+        int abs1 = Math.abs(o1);
+        int abs2 = Math.abs(o2);
 
-            if (first_abs == second_abs)
-                return o1 > o2 ? 1 : -1;
-            else
-                return first_abs - second_abs;
-        }));
-
-        for (int i=0; i<N; i++){
-            int now = scanner.nextInt();
-
-            if(now ==0){
-                if (priorityQueue.isEmpty()) {
-                    bw.write("0\n");
-                }
-                else {
-                    bw.write(priorityQueue.poll()+"\n");
-                }
-            }else {
-                priorityQueue.add(now);
-            }
+        if(abs1 != abs2){
+          return abs1 - abs2;
+        }else{
+          return o1 - o2;
         }
+      });
 
-        bw.flush();
-        bw.close();
-
+      for(int i=0; i<n; i++){
+        int input = scan.nextInt();
+        if(input==0){
+          if(pq.isEmpty()) System.out.println(0);
+          else System.out.println(pq.poll());
+        }else{
+          pq.add(input);
+        }
+      }
     }
+}
+
+class pqComparator implements Comparator<Integer> {
+  @Override
+  public int compare(Integer o1, Integer o2) {
+    int abs1 = Math.abs(o1);
+    int abs2 = Math.abs(o2);
+
+    if(abs1 != abs2){
+      return abs1 - abs2;
+    }else{
+      return o1 - o2;
+    }
+  }
 }
