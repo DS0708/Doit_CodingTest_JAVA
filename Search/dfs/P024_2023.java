ad31dfa35c749fedcdce5d7a5982549e0b974a41
DS0708 <tlsehdtjd070@naver.com>
2024-06-13 21:43:13 +0900

    DFS 기본 - 소수구하기

--- Search/dfs/P024_2023.java
+++ Search/dfs/P024_2023.java
@@ -1,36 +1,36 @@
 package Search.dfs;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class P024_2023 {
-    static int n;
-    public static void main(String[] args) {
-        n = new Scanner(System.in).nextInt();
+  static int n;
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    n = scan.nextInt();
 
-        DFS(2,1);
-        DFS(3,1);
-        DFS(5,1);
-        DFS(7,1);
-    }
+    DFS(2,1);
+    DFS(3,1);
+    DFS(5,1);
+    DFS(7,1);
+  }
 
-    public static void DFS(int num, int jarisu){
-        if(jarisu == n){
-            if(isPrime(num)){
-                System.out.println(num);
-                return;
-            }
-        }
-        for (int i=1; i<=9; i+=2){
-            if(isPrime(num*10+i)){
-                DFS (num*10 + i, jarisu+1);
-            }
-        }
+  static void DFS(int num, int scale){
+    if (scale == n){
+      System.out.println(num);
+      return;
+    }else{
+      for(int i=1; i<=9; i+=2){
+        int newNum = num * 10 + i;
+        if(isPrime(newNum)) DFS(newNum,scale+1);
+      }
     }
+  }
 
-    public static Boolean isPrime(int num){
-        for (int i=2; i<=num/2; i++){
-            if(num%i == 0) return false;
-        }
-        return true;
+  static boolean isPrime(int num){
+    for(int i=2; i<=Math.sqrt(num) ; i++){
+      if(num % i ==0) return false;
     }
+    return true;
+  }
+
 }
