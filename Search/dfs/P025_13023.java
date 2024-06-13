26f70899e5a7001a8379ebc03f3ac53a6b989095
DS0708 <tlsehdtjd070@naver.com>
2024-06-13 22:12:44 +0900

    DFS - 그래프에서 트리 찾는 알고리즘

--- Search/dfs/P025_13023.java
+++ Search/dfs/P025_13023.java
@@ -7,56 +7,54 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class P025_13023 {
-    static ArrayList<Integer>[] graph;
-    static boolean arrive;
-    static Boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+  static ArrayList<Integer>[] graph;
+  static boolean[] visited;
+  static boolean check;
+  static int V;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        arrive = false;
-        visited = new Boolean[V];
+    V = Integer.parseInt(st.nextToken());
+    int E = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[V];
-        for (int i=0; i<V; i++){
-            graph[i] = new ArrayList<>();
-            visited[i] = false;
-        }
-
-        for (int i=0; i<E; i++){
-            st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
-
-            graph[v1].add(v2);
-            graph[v2].add(v1);
-        }
-
-        for (int i=0; i<V; i++){
-            DFS (i,1);
-
-            if (arrive) break;
-        }
-
-       if(arrive) System.out.println(1);
-       else System.out.println(0);
+    graph = new ArrayList[V];
+    check = false;
+    visited = new boolean[V];
+    for(int i=0; i<V; i++){
+      graph[i] = new ArrayList<>();
     }
 
-    public static void DFS(int now, int depth){
-        if (depth ==5){
-            arrive = true;
-            return;
-        }
-        visited[now] = true;
-
-        for (int i : graph[now]){
-            if (visited[i] == false){
-                DFS(i,depth+1);
-            }
-        }
-
-        visited[now] = false;
+    for(int i=0; i<E; i++){
+      st = new StringTokenizer(br.readLine());
+      int e1 = Integer.parseInt(st.nextToken());
+      int e2 = Integer.parseInt(st.nextToken());
+      graph[e1].add(e2);
+      graph[e2].add(e1);
     }
+
+    for(int i=0; i<V; i++){
+      DFS(i,1);
+      if(check) break;
+    }
+
+    if(check) System.out.println(1);
+    else System.out.println(0);
+  }
+
+  public static void DFS(int now, int depth){
+    if(depth==V || check){
+      check = true;
+      return;
+    }
+
+    visited[now] = true;
+
+    for(int i : graph[now]){
+      if(!visited[i]) DFS(i,depth+1);
+    }
+
+    //나오면서 다시 방문하지 않은 척 해야함
+    visited[now] = false;
+  }
 }
