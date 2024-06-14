53c38565fcd56fca50b2ae6677bac7ebb1e1b1e7
DS0708 <tlsehdtjd070@naver.com>
2024-06-15 00:49:23 +0900

    Greedy - 제일 작은 수 만들기

--- Greedy/P036_1541.java
+++ Greedy/P036_1541.java
@@ -3,26 +3,24 @@ package Greedy;
 import java.util.Scanner;
 
 public class P036_1541 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String formula = scanner.nextLine();
-        String[] minus = formula.split("-");
-        boolean first = false;
-        long result = 0;
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String str = scan.next();
+    String[] strArray = str.split("-");
+    long result = 0;
 
-        for (String s : minus) {
-            String[] plus = s.split("\\+");
-            int sum=0;
-            for (String str : plus) sum += Integer.parseInt(str);
-            if (first == false){
-                result += sum;
-                first = true;
-                continue;
-            }
-            result -= sum;
-        }
-
-        System.out.println(result);
-
+    for(int i=0; i<strArray.length; i++){
+      //split는 정규 표현식을 인자로 받으며 "+"문자는 "하나 이상"이라는 의미로 사용되기 때문에 이스케이프 처리 []를 해줘야한다.
+      //이스케이프 처리 예 : \,^,$ ~~ 그냥 에러나면 이스케이프 처리하자... 언제 다외우냐 ...
+      String[] plusStr = strArray[i].split("[+]");
+      int num = 0;
+      for(int j=0; j<plusStr.length; j++){
+        num += Integer.parseInt(plusStr[j]);
+      }
+      if(i==0) result+=num;
+      else result -= num;
     }
+
+    System.out.println(result);
+  }
 }
