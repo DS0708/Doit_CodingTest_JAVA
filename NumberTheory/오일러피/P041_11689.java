d8014afb2bccb474d0f8b2de955f3af0a24e9ba5
DS0708 <tlsehdtjd070@naver.com>
2024-06-15 04:38:05 +0900

    오일러의 피 - 1~n까지의 수 중 n과 서로소인 수의 개수 구하기 - 솔직히 뭔지 모르겠음

--- NumberTheory/오일러피/P041_11689.java
+++ NumberTheory/오일러피/P041_11689.java
@@ -3,16 +3,26 @@ package NumberTheory.오일러피;
 import java.util.Scanner;
 
 public class P041_11689 {
-    public static void main(String[] args) {
-        long N = new Scanner(System.in).nextLong();
-        long result = N;
-        for (long i=2; i<=Math.sqrt(N);i++){ //제곱근까지만 진행
-            if (N%i ==0){                    //i가 소수인지 확인하기
-                result = result - result/i; // 결과값 업데이트 -> 오일러 피 공식
-                while (N%i==0) N /= i;      // 2^2*5 이면 2^2를 없애고 5만 남김
-            }
-        }
-        if(N>1) result = result - result/N;
-        System.out.println(result);
-    }
+  public static void main(String[] args) {
+   Scanner scan = new Scanner(System.in);
+   long n = scan.nextLong();
+   long result = n;
+   long p = 2;
+
+   while( p * p <= n){
+     if(n%p==0){
+       result -= result/p;
+       while(n%p == 0){
+         n /= p;
+       }
+     }
+     p++;
+   }
+
+   if(n > 1){
+     result -= result/n;
+   }
+
+   System.out.println(result);
+  }
 }
