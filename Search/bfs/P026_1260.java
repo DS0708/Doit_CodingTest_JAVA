69f54ffbc1bbf3a0cfe7a5594c2292b35d3bba2a
DS0708 <tlsehdtjd070@naver.com>
2024-06-14 19:40:25 +0900

    DFS, BFS 구현하고 탐색 순서 출력하기

--- Search/bfs/P026_1260.java
+++ Search/bfs/P026_1260.java
@@ -1,79 +1,73 @@
 package Search.bfs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class P026_1260 {
-    static List<Integer> graph[];
-    static boolean BFS_visited[];
-    static boolean DFS_visited[];
-    public static void main(String[] args) throws IOException {
-        //init
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+  static BufferedWriter bw;
+  static ArrayList<Integer>[] graph;
+  static boolean[] visited;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int start_V = Integer.parseInt(st.nextToken());
+    int V = Integer.parseInt(st.nextToken());
+    int E = Integer.parseInt(st.nextToken());
+    int start = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[V+1];
-        BFS_visited = new boolean[V+1];
-        DFS_visited = new boolean[V+1];
-
-        for (int i=1; i<=V; i++){
-            graph[i] = new ArrayList<>();
-            BFS_visited[i] = false;
-            DFS_visited[i] = false;
-        }
-
-        //graph에 Edge 삽입
-        for(int i=0; i<E; i++){
-            st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
-            graph[v1].add(v2);
-            graph[v2].add(v1);
-        }
-
-        //graph 오름차순 정렬
-        for (int i=1; i<=V; i++){
-            graph[i].sort(Comparator.naturalOrder());
-        }
-        //DFS 실행
-        DFS(start_V);
-
-        System.out.println();
-
-        //BFS 실행
-        BFS(start_V);
+    graph = new ArrayList[V+1];
+    for(int i=1; i<=V; i++){
+      graph[i] = new ArrayList<>();
     }
 
-    public static void DFS(int node){
-        System.out.print(node+" ");
-        DFS_visited[node] = true;
-
-        for (int i : graph[node]){
-            if (DFS_visited[i] == false){
-                DFS(i);
-            }
-        }
+    for(int i=0; i<E; i++){
+      st = new StringTokenizer(br.readLine());
+      int e1 = Integer.parseInt(st.nextToken());
+      int e2 = Integer.parseInt(st.nextToken());
+      graph[e1].add(e2);
+      graph[e2].add(e1);
     }
 
-    public static void BFS(int start_V){
-        Queue<Integer> queue = new LinkedList<>();
-
-        queue.add(start_V);
-
-        while (queue.isEmpty() == false){
-            int poll = queue.poll();
-            if (BFS_visited[poll] == false){
-                System.out.print(poll+" ");
-                BFS_visited[poll] = true;
-                for (int j : graph[poll]) queue.add(j);
-            }
-        }
+    for(int i=1; i<=V; i++){
+      Collections.sort(graph[i]);
     }
 
+    visited = new boolean[V+1];
+    DFS(start);
+    bw.write("\n");
+    visited = new boolean[V+1];
+    BFS(start);
+
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+  static void DFS(int node) throws IOException{
+    bw.write(node+" ");
+    visited[node] = true;
+    for(int i : graph[node]){
+      if(!visited[i]) DFS(i);
+    }
+  }
+  static void BFS(int node) throws IOException{
+    Queue<Integer> queue = new ArrayDeque<>();
+    queue.add(node);
+    visited[node] = true;
+    bw.write(node+" ");
+
+    while(!queue.isEmpty()){
+      int searchNode = queue.poll();
+      for(int i : graph[searchNode]){
+        if(!visited[i]) {
+          queue.add(i);
+          visited[i] = true;
+          bw.write(i+" ");
+        }
+      }
+    }
+
+  }
 }
