51b0f12a469cb186ed30b920221df6c052343ba0
DS0708 <tlsehdtjd070@naver.com>
2024-06-15 13:02:43 +0900

    1으로 이루어져 있는 수의 최대공약수 구하기

--- NumberTheory/유클리드호제법/P043_1850.java
+++ NumberTheory/유클리드호제법/P043_1850.java
@@ -8,20 +8,21 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class P043_1850 {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        long A = Long.parseLong(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
-        long gcd = gcd(A,B);
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    long n1 = Long.parseLong(st.nextToken());
+    long n2 = Long.parseLong(st.nextToken());
+    long gcdN = gcd(Math.max(n1,n2), Math.min(n1,n2));
 
-        for (long i=0; i<gcd; i++) bw.write(1+"");
-
-        bw.flush();
-        bw.close();
-        br.close();
+    for(int i=0; i<gcdN; i++){
+      bw.write("1");
     }
+    bw.flush();
+    bw.close();
+    br.close();
+  }
 
     public static long gcd(long n1, long n2){
         if (n2 ==0 ) return n1;
