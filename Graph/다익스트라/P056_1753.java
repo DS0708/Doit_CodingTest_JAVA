22ffc9082fd6ee7e7e9c359c1f38142f74144bd6
DS0708 <tlsehdtjd070@naver.com>
2024-06-16 14:54:43 +0900

    다익스트라 - 시작점이 주어지고, 비용이 모두 양수일 때, 그리디에 기반한 다익스트라를 사용해 시작점으로부터 모든 노드까지의 최단거리 구하기

--- Graph/다익스트라/P056_1753.java
+++ Graph/다익스트라/P056_1753.java
@@ -1,71 +1,81 @@
 package Graph.다익스트라;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class P056_1753 {
+  static int[] D;
+  static ArrayList<Edge1753>[] graph;
+  static boolean[] visited;
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     int V = Integer.parseInt(st.nextToken());
     int E = Integer.parseInt(st.nextToken());
-    int startNode = Integer.parseInt(br.readLine());
-    int[] distance = new int[V+1];
-    boolean[] visited = new boolean[V+1];
-    ArrayList<ArrayList<Edge_1753>> edgeList = new ArrayList<>();
-    PriorityQueue<Edge_1753> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.value,o2.value));
+    int start = Integer.parseInt(br.readLine());
 
-    for(int i=0; i<=V; i++){
-      edgeList.add(new ArrayList<>());
-      distance[i] = Integer.MAX_VALUE;
+    D = new int[V+1];
+    graph = new ArrayList[V+1];
+    visited = new boolean[V+1];
+    for(int i=1; i<=V; i++){
+      graph[i] = new ArrayList<>();
+      D[i] = Integer.MAX_VALUE;
     }
 
-    for(int i=1; i<=E; i++){
+    for(int i=0; i<E; i++){
       st = new StringTokenizer(br.readLine());
       int s = Integer.parseInt(st.nextToken());
       int e = Integer.parseInt(st.nextToken());
       int v = Integer.parseInt(st.nextToken());
-      edgeList.get(s).add(new Edge_1753(e,v));
+      graph[s].add(new Edge1753(e,v));
     }
 
-    //Dijkstra
-    pq.offer(new Edge_1753(startNode,0));
-    distance[startNode] = 0;
+    dijkstra(start);
+
+    for(int i=1; i<=V; i++){
+      if(visited[i]) System.out.println(D[i]);
+      else System.out.println("INF");
+    }
+
+  }
+
+  static void dijkstra(int start){
+    //다익스트라 기본 로직 :
+    //큐에 있는 노드 중, 갈 수 있는 비용이 제일 적은 노드를 선택 (그리디)
+    //그 노드에 간 후, 방문했다고 표시한다.
+    //그 노드에서 방문할 수 있는 노드들을 살펴본다.(살펴보는게 중요함)
+    //살펴본 다음에 최소 비용으로 갈 수 있는 노드들만 큐에 담고 최소거리 배열(D)를 초기화해준다.
+    //이때 중요한 것은 (지금까지 왔던 비용) + (현재 노드에서 새로운 노드로 가는 비용) 을 더한 값을 에지의 비용으로 담는다.
+    //쉽게 생각하면, 큐에 담아지는 노드와 비용은, 시작점부터 그 노드까지의 비용이 하나의 에지라고 생각하고 담는 것
+    //예를 들자면, "시작점->중간노드->중간노드->목표노드"의 에지를 담는 것이 아닌 "시작점->목표노드"라고 생각하고 담는 것 !!
+    PriorityQueue<Edge1753> pq = new PriorityQueue<>();
+    D[start] = 0;
+    //시작점을 첫 노드로 0만큼 간다고 생각해야함
+    pq.offer(new Edge1753(start,0));
+
     while(!pq.isEmpty()){
-      Edge_1753 now = pq.poll();
-      int nowNode = now.node;
-      int nowDistacne = now.value;  //시작노드에서 현재노드까지의 값이 큐에 추가되므로 nowDistance라고 표현
-
-      if (visited[nowNode]) continue;
-      visited[nowNode] = true;
-
-      for(Edge_1753 next : edgeList.get(nowNode)){
-        int nextNode = next.node;
-        int nextValue = next.value;
-        if(distance[nextNode] > nowDistacne + nextValue){
-          distance[nextNode] = nowDistacne + nextValue;
-          pq.add(new Edge_1753(nextNode, distance[nextNode]));
-          //시작노드에서 다음노드까지의 에지를 하나라고 생각하고 추가해주기, ex) 1 -> 3 -> 4 를 그냥 1번에서 4번으로 가는 엣지 하나라고 생각하기
+      Edge1753 current = pq.poll();
+      visited[current.node] = true;
+      for(Edge1753 next : graph[current.node]) {
+        int targetVal = D[current.node]+next.val;
+        if(!visited[next.node] && targetVal < D[next.node]) {
+          D[next.node] = targetVal;
+          pq.offer(new Edge1753(next.node, targetVal));
         }
       }
     }
-
-    //print Result
-    for(int i=1; i<=V; i++){
-      if (visited[i]) System.out.println(distance[i]);
-      else System.out.println("INF");
-    }
   }
-}
 
-class Edge_1753{
-  int node;
-  int value;
-  Edge_1753(int node,int value){
-    this.node = node;
-    this.value = value;
+  static class Edge1753 implements Comparable<Edge1753>{
+    int node;
+    int val;
+    Edge1753(int node, int val){
+      this.node = node;
+      this.val = val;
+    }
+    @Override
+    public int compareTo(Edge1753 t){
+      return this.val - t.val;
+    }
   }
 }
