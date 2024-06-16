61b0c8a5be1ab48301de54116fa0f5a78ab90242
DS0708 <tlsehdtjd070@naver.com>
2024-06-16 13:29:04 +0900

    유니온 파인드 - 같은 집합에 속해있는지 여부 판단하기

--- Graph/유니온파인드/P050_1717.java
+++ Graph/유니온파인드/P050_1717.java
@@ -1,50 +1,49 @@
 package Graph.유니온파인드;
 
-import java.io.BufferedReader;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class P050_1717 {
-  static int parent[];
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-    int m = scanner.nextInt();
-    parent = new int[n+1];
+  static int[] parent;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int V = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    parent = new int[V+1];
+    for(int i=1; i<=V; i++){
+      parent[i] = i;
+    }
 
-    for(int i=0; i<=n ; i++) parent[i] = i;
+    for(int i=0; i<m; i++){
+      st = new StringTokenizer(br.readLine());
+      int q = Integer.parseInt(st.nextToken());
+      int n1 = Integer.parseInt(st.nextToken());
+      int n2 = Integer.parseInt(st.nextToken());
 
-    for (int i=0; i<m; i++){
-      int question = scanner.nextInt();
-      int a = scanner.nextInt();
-      int b = scanner.nextInt();
-
-      if (question == 0) union(a,b);
-      else {
-        if (checkSum(a,b)) System.out.println("YES");
+      if(q==1){
+        if(find(n1)==find(n2)) System.out.println("YES");
         else System.out.println("NO");
+      }else{
+        union(n1,n2);
       }
     }
   }
 
-  static int find(int num){
-    if (num == parent[num]) return num;
-    else {
-//      parent[num] = find(parent[num]);
-//      return num;
-      return parent[num] = find(parent[num]);
+  static void union(int n1, int n2){
+    int a = find(n1);
+    int b = find(n2);
+
+    if(a!=b){
+      parent[b] = a;
     }
   }
 
-  static void union(int a, int b){
-    a = find(a);
-    b = find(b);
-    if (a!=b) parent[b] = a;
+  static int find(int n){
+    if(parent[n]==n) return n;
+    else{
+      return parent[n] = find(parent[n]);
+    }
   }
 
-  static boolean checkSum(int a, int b){
-    a = find(a);
-    b = find(b);
-    if (a==b) return true;
-    else return false;
-  }
 }
