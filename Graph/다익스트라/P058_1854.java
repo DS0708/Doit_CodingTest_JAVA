a8622bb9169434da194ab48ff3faa09ccd81fe20
DS0708 <tlsehdtjd070@naver.com>
2024-06-16 21:54:11 +0900

    다익스트라 - 모든 노드에 대하여 시작점으로부터 k번쨰의 최단 경로 구하기

--- Graph/다익스트라/P058_1854.java
+++ Graph/다익스트라/P058_1854.java
@@ -1,82 +1,76 @@
 package Graph.다익스트라;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class P058_1854 {
+  static class Edge1854{
+    int node;
+    int val;
+    Edge1854(int node, int val){
+      this.node = node;
+      this.val = val;
+    }
+  }
+  static ArrayList<Edge1854>[] graph;
+  static PriorityQueue<Integer>[] Dpq;
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
-    int k = Integer.parseInt(st.nextToken());
-    PriorityQueue<Integer>[] distPQ = new PriorityQueue[n+1];
-    int[][] graph = new int[n+1][n+1];
-    PriorityQueue<Edge_1854> pq = new PriorityQueue<>();
-    Comparator<Integer> comparator = new Comparator<Integer>() {
-      @Override
-      public int compare(Integer o1, Integer o2) {
-        return o2.compareTo(o1);  //내림차순 정렬 : poll하면 큰 수 먼저 나옴
-      }
-    };
-    for(int i=0; i<=n ;i++){
-      distPQ[i] = new PriorityQueue<>(k,comparator);    //k개의 크기만큼의 우선순위 큐 생성
+    int V = Integer.parseInt(st.nextToken());
+    int E = Integer.parseInt(st.nextToken());
+    int Dpqsize = Integer.parseInt(st.nextToken());
+
+    graph = new ArrayList[V+1];
+    Dpq = new PriorityQueue[V+1];
+    for(int i=1; i<=V; i++){
+      graph[i] = new ArrayList<>();
+      Dpq[i] = new PriorityQueue<>(Dpqsize,(o1, o2)->{
+        return o2 - o1;
+      });
     }
-    //graph 입력
-    for(int i=0; i<m; i++){
+
+    for(int i=0; i<E; i++){
       st = new StringTokenizer(br.readLine());
       int s = Integer.parseInt(st.nextToken());
       int e = Integer.parseInt(st.nextToken());
       int v = Integer.parseInt(st.nextToken());
-      graph[s][e] = v;
+      graph[s].add(new Edge1854(e,v));
     }
 
-    //다익스트라
-    pq.offer(new Edge_1854(1,0));
-    distPQ[1].add(0);
+    dijkstra(1,Dpqsize);
+
+    for(int i=1; i<=V; i++){
+      if(Dpq[i].size() == Dpqsize) bw.write(Dpq[i].poll()+"\n");
+      else bw.write(-1+"\n");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+  static void dijkstra(int start, int Dpqsize){
+    PriorityQueue<Edge1854> pq = new PriorityQueue<>((o1,o2)->{
+      return o1.val - o2.val;
+    });
+    pq.offer(new Edge1854(start,0));
+    Dpq[start].offer(0);
+
     while(!pq.isEmpty()){
-      Edge_1854 now = pq.poll();
-      int nowNode = now.targetNode;
-      int nowValue = now.value;
-      for(int target=1; target<=n ; target++){  //시간복잡도를 위하여 graph를 matrix로 구성
-        if(graph[nowNode][target] != 0) {       //0이 아니면 에지가 존재
-          //distPQ에 들어가지 않는 경우는 k번째까지의 최단거리 비용보다다 더 많은 비용이므로, 그거 보다 더 적은 비용으로 갈 수 있는 k개의 경로가 존재한다. 즉 볼 필요가 없다.
-          int val = nowValue + graph[nowNode][target];    //nowNode를 거쳐 target까지 가는 value
-          if (distPQ[target].size() < k) {            //distPQ의 크기가 k보다 작으면 무조건 추가해주면 됨
-            distPQ[target].offer(val);
-            pq.offer(new Edge_1854(target, val));
-          } else {                                    //distPQ의 크기가 k일 경우 제일 큰 값(내림차순 정렬했으니 peek값)과 비교해서 작을 경우만 넣어주면 된다.
-            int peek = distPQ[target].peek();
-            if (peek > val) {
-              distPQ[target].poll();
-              distPQ[target].offer(val);
-              pq.offer(new Edge_1854(target, val));
-            }
-          }
-
-        }//if
-      }//for
+      Edge1854 current = pq.poll();
+      for(Edge1854 next : graph[current.node]){
+        int nextVal = current.val + next.val;
+        if(Dpq[next.node].size() < Dpqsize){
+          Dpq[next.node].offer(nextVal);
+          pq.offer(new Edge1854(next.node, nextVal));
+        }else if(Dpq[next.node].peek() > nextVal){
+          Dpq[next.node].poll();
+          Dpq[next.node].offer(nextVal);
+          pq.offer(new Edge1854(next.node, nextVal));
+        }
+      }
     }
-
-    //결과 출력
-    for(int i=1; i<=n ; i++){
-      if(distPQ[i].size() < k) System.out.println(-1);
-      else System.out.println(distPQ[i].peek());
-    }
-  }
-}
-class Edge_1854 implements Comparable<Edge_1854>{
-  int targetNode;
-  int value;
-  Edge_1854(int targetNode, int value){
-    this.targetNode = targetNode;
-    this.value = value;
-  }
-  @Override
-  public int compareTo(Edge_1854 o) {
-    return this.value < o.value ? -1 : 1; //오름차순 정렬, poll 하면 작은 거 먼저 나옴
   }
 }
