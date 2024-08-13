042ec999095c63932adeeb7d02b9456dc5dc51b8
DS0708 <jonum12312@gmail.com>
2024-08-13 16:22:14 +0900

    버블 정렬 - iteration 2

--- Sort/버블정렬/P015_2750.java
+++ Sort/버블정렬/P015_2750.java
@@ -1,30 +1,28 @@
 package Sort.버블정렬;
 
-import java.util.Scanner;
+import java.util.*;
+
 
 public class P015_2750 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n+1];
 
-        int N = scanner.nextInt();
-        int A[] = new int[N];
-
-        for (int i=0; i<N; i++){
-            A[i] = scanner.nextInt();
+        for(int i=1; i<=n; i++){
+            arr[i] = scan.nextInt();
         }
 
-        for (int i=0; i<N-1; i++){
-            for (int j=0; j<N-1-i; j++){
-                if (A[j] > A[j+1]){
-                    int temp = A[j];
-                    A[j] = A[j+1];
-                    A[j+1] = temp;
+        for(int i=1; i<=n-1; i++){
+            for(int j=1; j<=n-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
 
-        for (int i=0; i<N; i++){
-            System.out.println(A[i]);
-        }
+        for(int i=1; i<=n ; i++) System.out.println(arr[i]);
     }
 }
