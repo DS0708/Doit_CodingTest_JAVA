15517558a127d16eeabca465cacf7660954dc59d
DS0708 <jonum12312@gmail.com>
2024-08-20 14:20:07 +0900

    Merge Sort로 bubble sort 스왑 개수 구하기 - iteration2

--- Sort/병합정렬/P021_1517.java
+++ Sort/병합정렬/P021_1517.java
@@ -1,71 +1,70 @@
 package Sort.병합정렬;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class P021_1517 {
-    static int[] A,temp;
-    static Long result;
+    static int[] arr,tmp;
+    static long res;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        A = new int[n];
-        temp = new int[n];
-        result = 0l;
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        for (int i=0; i<n; i++){
-            A[i] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        tmp = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        merge_sort(0,n-1);
+        res = 0;
 
-        bw.write(result+"");
-        bw.flush();
-        bw.close();
+        mergeSort(1,N);
+
+        System.out.println(res);
+
         br.close();
     }
-    public static void merge_sort(int start, int end){
-        if (end - start < 1) return;
 
-        int mid = (start + end)/2;
+    static void mergeSort(int s, int e){
+        if(s==e) return;
 
-        merge_sort(start,mid);
-        merge_sort(mid+1,end);
+        int mid = (s+e)/2;
 
-        for(int i=start; i<=end; i++){
-            temp[i] = A[i];
-        }
+        mergeSort(s,mid);
+        mergeSort(mid+1,e);
 
-        int current = start;
-        int index1 = start;
-        int index2 = mid+1;
+        int i = s;
+        int j = mid+1;
+        int index = s;
 
-        while(index1 <= mid && index2 <= end){
-            if (temp[index1] > temp[index2]){
-                A[current] = temp[index2];
-                //첫번쨰는 원래 index보다 왼쪽으로 이동할 수 없으므로
-                //index2에 있는 것만 더해주면 된다.
-                result += index2 - current;
-                current++;
-                index2++;
-            }else {
-                A[current] = temp[index1];
-                current++;
-                index1++;
+        while(i<=mid && j<=e){
+            if(arr[i]<=arr[j]){
+                tmp[index] = arr[i];
+                i++;
+                index++;
+            }else{
+                tmp[index] = arr[j];
+                res += (j-index);
+                j++;
+                index++;
             }
         }
-        while (index1 <= mid){
-            A[current] = temp[index1];
-            current++;
-            index1++;
+        while(i<=mid){
+            tmp[index] = arr[i];
+            i++;
+            index++;
         }
-        while (index2 <= end) {
-            A[current] = temp[index2];
-            result += index2 - current;
-            current++;
-            index2++;
+        while(j<=e){
+            tmp[index] = arr[j];
+            res += (j-index);
+            j++;
+            index++;
         }
+
+        for(int k=s; k<=e; k++) arr[k] = tmp[k];
     }
+
 }
