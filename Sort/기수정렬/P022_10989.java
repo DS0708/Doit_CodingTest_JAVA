360c21a30ddb2a54a05df7b5ab77bb43b01d5ad6
DS0708 <jonum12312@gmail.com>
2024-08-21 15:51:21 +0900

    Radix Sort - iteration2

--- Sort/기수정렬/P022_10989.java
+++ Sort/기수정렬/P022_10989.java
@@ -1,61 +1,58 @@
 package Sort.기수정렬;
 
 import java.io.*;
+import java.util.*;
 
 public class P022_10989 {
+    static int N;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] A = new int[n];
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
 
-        for (int i=0; i<n ;i++){
-            A[i] = Integer.parseInt(br.readLine());
-        }
+        for(int i=1 ;i<=N; i++)  arr[i] = Integer.parseInt(br.readLine());
 
-        Radix_sort(A,5);
+        radixSort(5);
 
-        for (int i=0; i<n; i++){
-            bw.write(A[i]+"\n");
-        }
+        for(int i=1 ;i<=N; i++)  bw.write(arr[i]+"\n");
 
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public static void Radix_sort(int[] A, int scaleNum){
-       int count = 1;
-       int scale = 1;
+    static void radixSort(int max){
+        int count = 0;
+        int jarisu = 1;
+        int[] temp = new int[N+1];
+        while(count!=max){
+            int[] bucket = new int[10];
 
-       while(count <= scaleNum){
-         int[] output = new int[A.length];
-         int[] bucket = new int[10];
+            for(int i=1; i<=N; i++){
+                int curNum = (arr[i]/jarisu)%10;
+                bucket[curNum]++;
+            }
 
-         //각 자리숫자가 현재 어디에 위치하는지 부분합을 통해 index를 뽑는다.
-         for(int i=0; i<A.length; i++){
-           int bucketIndex = A[i]/scale%10;
-           bucket[bucketIndex]++;
-         }
-         for(int i=1; i<10; i++){
-           bucket[i] += bucket[i-1];
-         }
-         //bucket은 현재 몇 개가 존재하는지에 대한 부분합이므로 결과의 Index와 동일함.
-         for(int i=A.length-1; i>=0; i--){
-           int bucketIndex = A[i]/scale%10;
-           int outputIndex = bucket[bucketIndex] - 1;
-           output[outputIndex] = A[i];
-           bucket[bucketIndex]--;
-         }
-         //자바에서 함수 호출은 Call by Value방식으로 깊은 복사를 해야지 A의 실제 객체에 접근할 수 있다.
-         for(int i=0; i<A.length; i++){
-           A[i] = output[i];
-         }
+            for(int i=1; i<10; i++){
+                bucket[i] += bucket[i-1];
+            }
 
-         count++;
-         scale *= 10;
-       }
+            for(int i=N; i>=1; i--){
+                int curNum = (arr[i]/jarisu)%10;
+                temp[bucket[curNum]] = arr[i];
+                bucket[curNum]--;
+            }
+
+            for(int i=1; i<=N; i++){
+                arr[i] = temp[i];
+            }
+
+            jarisu*=10;
+            count++;
+        }
     }
 
 }
