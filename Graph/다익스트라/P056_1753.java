fffd8bcaea7a19f76eca75c5e098e99db73babac
DS0708 <jonum12312@gmail.com>
2024-09-13 13:50:46 +0900

    다익스트라 알고리즘

--- Graph/다익스트라/P056_1753.java
+++ Graph/다익스트라/P056_1753.java
@@ -1,7 +1,5 @@
 package Graph.다익스트라;
 
-import Search.bfs.Edge;
-
 import java.io.*;
 import java.util.*;
 
@@ -11,16 +9,17 @@ public class P056_1753 {
   static boolean[] visited;
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] input = br.readLine().split(" ");
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
     //input
-    int V = Integer.parseInt(input[0]);
-    int E = Integer.parseInt(input[0]);
+    int V = Integer.parseInt(st.nextToken());
+    int E = Integer.parseInt(st.nextToken());
     int K = Integer.parseInt(br.readLine());
 
     int max = (int)1e6; //가중치 최대 * 에지의 최대 갯수  보다 큰 수
 
     graph = new ArrayList[V+1];
-    Arrays.fill(graph,new ArrayList<>());
+    for(int i=1; i<=V ; i++) graph[i] = new ArrayList<>();
 
     visited = new boolean[V+1];
 
@@ -29,10 +28,10 @@ public class P056_1753 {
     DP[K] = 0;
 
     for(int i=1; i<=E; i++){
-      input = br.readLine().split(" ");
-      int s = Integer.parseInt(input[0]);
-      int e = Integer.parseInt(input[1]);
-      int w = Integer.parseInt(input[2]);
+      st = new StringTokenizer(br.readLine());
+      int s = Integer.parseInt(st.nextToken());
+      int e = Integer.parseInt(st.nextToken());
+      int w = Integer.parseInt(st.nextToken());
       graph[s].add(new Edge1753(e,w));
     }
 
@@ -41,27 +40,35 @@ public class P056_1753 {
 
     //output
     for(int i=1; i<=V; i++) {
-      if(DP[i]==max) System.out.println("INF");
-      else System.out.println(DP[i]);
+      if(DP[i]==max) bw.write("INF\n");
+      else bw.write(DP[i]+"\n");
     }
+    bw.flush();
+    bw.close();
+    br.close();
   }
 
-  static void dijkstra(int start){
+  static void dijkstra(int start) {
     PriorityQueue<Edge1753> pq = new PriorityQueue<>((o1,o2)->o1.weight-o2.weight);
     pq.add(new Edge1753(start,0));
-    visited[start] = true;
 
     while(!pq.isEmpty()){
       Edge1753 cur = pq.poll();
-      int curNode = cur.node;
-      int curWeight = cur.weight;
-      for(Edge1753 edge : graph[curNode]){
-        if(!visited[edge.node] && curWeight+edge.weight < DP[edge.node]){
-          visited[edge.node] = true;
-          DP[edge.node] = curWeight+edge.weight;
-          pq.add(new Edge1753(edge.node,DP[edge.node]));
+      //visited가 true라면 현재 값보다 더 작은 weight로 나온적이 있는 노드이다.
+      //그때그때 최선의 선택을 하기 때문에, 나중에 개척된 길을 통해 이미 지나간 노드에 대하여 최소 weight는 나오는 경우가 없다.
+      //만약 있다면, 그것은 음수 가중치가 존재하는 것임.
+      if(visited[cur.node]) continue; //그래서 이미 방문한 곳은 안해도 된다.
+      visited[cur.node] = true;
+      //현재 노드로 선택된 곳은, 최소 길이가 보장되므로
+      // 여기서 갈 수 있는 곳 중 방문하지 않았고
+      // DP의 길이가 갱신되는 곳이라면 pq에 추가
+      for(Edge1753 next : graph[cur.node]){
+        if(!visited[next.node] && cur.weight+next.weight < DP[next.node]){
+          DP[next.node] = cur.weight+next.weight;
+          pq.add(new Edge1753(next.node,DP[next.node]));
         }
       }
+
     }
 
   }
